95a64b18a39db5f671b6be4d601ff0efa6ed6c3f
Sujan Gautam <68267227+sujan66root@users.noreply.github.com>
2024-02-14 20:27:33 -0500

    Assignment 2 added

---
+++ Assignment2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+
+            Console.Write("Enter a real number: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            if ((x >= 2 && x < 3) || (x >= 0 && x <= 1) || (x >= -10 && x <= -2))
+            {
+                Console.WriteLine($"{x} x belongs to I");
+            }
+            else
+            {
+                Console.WriteLine($"{x} x does not belong to I");
+            }
+            Console.ReadKey();
+        }
+    }
+}
